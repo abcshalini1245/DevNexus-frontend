462cbbfc4e228b6c2ad053c4665497bdb829d76a
Shalini Upadhyay <abcshalini1245@gmail.com>
2026-07-06 23:23:14 +0530

    Final stable version before deployment

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -291,11 +291,11 @@ setPreviewImage(user.photourl || "");
   </div>
 
   {/* URL Input */}
-  <label className="form-control w-full">
+  <label className="form-control w-full ">
 
     <div className="label">
-      <span className="label-text font-medium">
-        Paste Image URL
+      <span className="label-text font-semibold p-4 mb-2">
+        Paste Image URL: 
       </span>
     </div>
 {/* 
@@ -462,18 +462,54 @@ setPreviewImage(user.photourl || "");
           {/* Location */}
 
           <label className="form-control mb-4">
-            <div className="label">
-              <span className="label-text">📍 Location</span>
-            </div>
+  <div className="label">
+    <span className="label-text">📍 State</span>
+  </div>
 
-            <input
-              type="text"
-              className="input input-bordered"
-              placeholder="Durgapur, West Bengal"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-          </label>
+  <select
+    className="select select-bordered w-full"
+    value={location}
+    onChange={(e) => setLocation(e.target.value)}
+  >
+    <option value="">Select State</option>
+    <option value="Andhra Pradesh">Andhra Pradesh</option>
+    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+    <option value="Assam">Assam</option>
+    <option value="Bihar">Bihar</option>
+    <option value="Chhattisgarh">Chhattisgarh</option>
+    <option value="Goa">Goa</option>
+    <option value="Gujarat">Gujarat</option>
+    <option value="Haryana">Haryana</option>
+    <option value="Himachal Pradesh">Himachal Pradesh</option>
+    <option value="Jharkhand">Jharkhand</option>
+    <option value="Karnataka">Karnataka</option>
+    <option value="Kerala">Kerala</option>
+    <option value="Madhya Pradesh">Madhya Pradesh</option>
+    <option value="Maharashtra">Maharashtra</option>
+    <option value="Manipur">Manipur</option>
+    <option value="Meghalaya">Meghalaya</option>
+    <option value="Mizoram">Mizoram</option>
+    <option value="Nagaland">Nagaland</option>
+    <option value="Odisha">Odisha</option>
+    <option value="Punjab">Punjab</option>
+    <option value="Rajasthan">Rajasthan</option>
+    <option value="Sikkim">Sikkim</option>
+    <option value="Tamil Nadu">Tamil Nadu</option>
+    <option value="Telangana">Telangana</option>
+    <option value="Tripura">Tripura</option>
+    <option value="Uttar Pradesh">Uttar Pradesh</option>
+    <option value="Uttarakhand">Uttarakhand</option>
+    <option value="West Bengal">West Bengal</option>
+    <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+    <option value="Chandigarh">Chandigarh</option>
+    
+    <option value="Delhi">Delhi</option>
+    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+    <option value="Ladakh">Ladakh</option>
+    <option value="Lakshadweep">Lakshadweep</option>
+    <option value="Puducherry">Puducherry</option>
+  </select>
+</label>
 
           {/* Education */}
 
